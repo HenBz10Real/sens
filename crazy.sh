9853bff17ae3b6fe6f9ab08ee5b0d829396eeeb1
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-30 00:51:03 +0700

    Update crazy.sh

--- crazy.sh
+++ crazy.sh
@@ -11,7 +11,7 @@ text="$1"
  done
 echo
 }
-local check_id=$(storm "r17rYI0tYD6Cp9erOIfuNZlP29kC0ycsbA6DKSJQNWjC3EcseDmrNIatdW6vJ50vYH=")
+local check_id=$(storm "r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2Mltp9gQXEpPOg5CeI4uR==")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
   echo ""
