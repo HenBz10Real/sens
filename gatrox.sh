800967ebbc8b53341cdde2a5c6512860d13bf2a6
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-30 00:40:45 +0700

    Update gatrox.sh

--- gatrox.sh
+++ gatrox.sh
@@ -19,7 +19,7 @@ if [ $check_vip = true ]; then
   echo 
   sleep 0.6
   echo
-  printer "Silahkan cek notifikasi anda, ${RED}berhasil terpasang"
+  echo "Silahkan cek notifikasi anda, ${RED}berhasil terpasang"
   sleep 0.6
   sensivity_calibrar() {
     local coordinate=$1
@@ -93,7 +93,7 @@ else
   echo ""
   sleep 0.6
   echo
-  printer "Invalid device, ${RED} file rusak ( buy to original script )"
+  echo "Invalid device, ${RED} file rusak ( buy to original script )"
   sleep 0.6
   sleep 2
   am start -a android.intent.action.VIEW -d ${link} > /dev/null 2>&1
