4be4f50559ca9f07ace1179763d15e8e1eb5bd6a
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-30 00:51:40 +0700

    Update gatrox.sh

--- gatrox.sh
+++ gatrox.sh
@@ -1,6 +1,6 @@
 source $FUNCTION
 RED="\033[31m"
-local check_id=$(storm "r17rYI0tYD6Cp9erOIfuNZlP29kC0ycsbA6DKSJQNWjC3EcseDmrNIePOIps3xluYy0")
+local check_id=$(storm "r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2Mltp9gQXuyuYAmvT50vYH=")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
   echo ""
