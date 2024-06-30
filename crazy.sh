e7798c4be83c9460f663b77f7ff4d7de88aaf2dd
HENVX PROJECT <141293896+HenBz10Real@users.noreply.github.com>
2024-06-30 11:26:49 +0700

    Update crazy.sh

--- crazy.sh
+++ crazy.sh
@@ -102,7 +102,7 @@ if [ $check_vip = true ]; then
   optimazion_freefire > /dev/null 2>&1
   sleep 2
   adaptive_sensivity() {
-    wm size 1152x2496
+    wm size 1160x2530
   }
   adaptive_sensivity > /dev/null 2>&1
 else
