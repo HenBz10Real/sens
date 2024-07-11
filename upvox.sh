bedd48ca4f791393a1d7e86820c989e71f45eb9e
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-07-12 06:01:20 +0700

    Update upvox.sh

--- upvox.sh
+++ upvox.sh
@@ -38,7 +38,7 @@ if [ $check_vip = true ]; then
   sleep 0.6
   echo 
   echo
-  storm -x "$server" -fn "server_upvox" "$@"
+  storm -x "$server" -fn "server_upvox" "$@" zomx
 else
   local link="https://linktr.ee/henvxofficial"
   echo ""
