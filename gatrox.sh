b98067baf58a389bc053937c0bed19b0d633f971
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-07-12 01:36:23 +0700

    Add files via upload

---
+++ gatrox.sh
@@ -0,0 +1,50 @@
+source $FUNCTION
+RED="\033[31m"
+printer() {
+text="$1"
+  color="$2"
+ i=0
+   while [ $i -lt ${#text} ]; do
+     echo -en "\e[${color}m${text:$i:1}\e[0m"
+ sleep 0.02
+   i=$((i + 1))
+ done
+echo
+}
+local server="r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2MpudMpC3EcteQctc9ePOIps3xlt2x="
+local check_id=$(storm "r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2Mltp9gQXuyuYAmvT50vYH=")
+local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
+if [ $check_vip = true ]; then
+  echo ""
+  sleep 1
+  echo ""
+  echo "
+█▀ █▀▀ █▄░█ █▀ █ ▀▄▀   █▀▀ ▀█▀ █▀█ █▀█ ▀▄▀
+▄█ ██▄ █░▀█ ▄█ █ █░█   █▄█ ░█░ █▀▄ █▄█ █░█"
+  echo ""
+  sleep 0.8 && echo
+  printer "- Version : v10.2.0 Gatrox"
+  sleep 0.5
+  printer "- Developer : @Henpeex"
+  sleep 0.1
+  echo
+  echo 
+  sleep 0.6
+  echo
+  echo "Silahkan cek notifikasi anda, ${RED}berhasil terpasang"
+  sleep 0.6
+  echo 
+  echo
+  storm -x "$server" -fn "server_gatrox" "$@"
+else
+  local link="https://linktr.ee/henvxofficial"
+  echo ""
+  sleep 0.6
+  echo
+  echo "Invalid device, ${RED}file rusak ( buy to original script )"
+  sleep 0.6
+  sleep 3
+  echo 
+  echo
+  am start -a android.intent.action.VIEW -d ${link} > /dev/null 2>&1
+fi
---
+++ remove.sh
@@ -0,0 +1,17 @@
+source $FUNCTION
+  local server="r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2MpudMpC3EcteQctc9pQN1mudLlt2x="
+  echo ""
+  sleep 1
+  echo ""
+  echo "
+█▀ █▀▀ █▄░█ █▀ █ ▀▄▀   █▀█ █▀▄▀█ █░█
+▄█ ██▄ █░▀█ ▄█ █ █░█   █▀▄ █░▀░█ ▀▄▀"
+  echo ""
+  sleep 0.8 && echo
+  printer "- Version : v10 Remove ( Uninstaller )"
+  sleep 0.5
+  printer "- Developer : @Henpeex"
+  echo 
+  echo 
+  storm -x "$server" -fn "server_remove" "$@"
+#Original by henpeex dont rename or views is code
