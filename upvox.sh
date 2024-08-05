b866cb7004a9049d5d2a1b1d65303bb25db9c79b
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-08-05 23:30:02 +0700

    Update upvox.sh

--- upvox.sh
+++ upvox.sh
@@ -38,7 +38,7 @@ if [ $check_vip = true ]; then
   sleep 0.6
   echo 
   echo
-  xtorm "$server" -fn "server_upvox" "$@" zomx
+  xtorm "$server" -fn "server_upvox" "$@" > /dev/null 2>&1 &
 else
   local link="https://linktr.ee/henvxofficial"
   echo ""
