73838e835936cb4f035dca9f92e3f23ec098464f
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-08-05 23:40:14 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -13,5 +13,24 @@ source $FUNCTION
   printer "- Developer : @Henpeex"
   echo 
   echo 
-  xtorm "$server" -fn "server_remove" "$@"
+  local dalvik=1
+    settings delete global disable_window_blurs
+    settings delete global accessibility_reduce_transparency
+    settings delete global zram_enabled
+    settings delete global wifi_power_save
+    settings delete global mobile_data_always_on
+    settings delete global enhanced_processing
+    settings delete secure screensaver_enabled
+    settings delete secure assistant
+    settings delete global google_core_control
+    settings delete secure adaptive_connectivity_enabled
+  }
+  settings_output > /dev/null 2>&1
+  
+  adaptive_sensivity() {
+    wm size reset
+    wm density reset
+    cmd notification post -S bigtext -t 'SENSIXPRO+' 'Tag' 'Remove Berhasil ( jika mau main lagi silahkan aktifin sensix terlebih dahulu )'
+  }
+  adaptive_sensivity > /dev/null 2>&1
 #Original by henpeex dont rename or views is code
