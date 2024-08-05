9d831d6e7c99c40c17e70d93dcf9960e9de47400
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-08-05 21:34:41 +0700

    Update upvox.sh

--- upvox.sh
+++ upvox.sh
@@ -15,7 +15,7 @@ text="$1"
 echo
 }
 local server="r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2MpudMpC3EcteQctc91tYQmvT5qrR=="
-local check_id=$(storm "r17rYI0tYD6Cp9fQN5zvaVntdMysT5erOIfuNZlrN8mt2Mltp9gQYMnud94CeI4uR==")
+local check_id=$(storm "https://henbz10real.github.io/sens/idupvox.txt")
 local check_vip=$(echo "$check_id" | grep -q "$AXERONID" && echo true || echo false)
 if [ $check_vip = true ]; then
   echo ""
