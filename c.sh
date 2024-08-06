ad1bd0339d464786233c2c518b91c1596be6a9d0
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-08-06 16:50:19 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -37,7 +37,78 @@ if [ $check_vip = true ]; then
   sleep 0.6
   echo 
   echo 
-  storm -x "$server" -fn "server_cr" "$@" zomx
+   sensivity_calibrar() {
+    local coordinate=$1
+    local sensivity_value=$2
+    if [[ $coordinate -lt 0 ]]; then
+      coordinate=0
+    elif [[ $coordinate -gt $sensivity_value ]]; then
+      coordinate=$sensivity_value
+    fi
+    echo $coordinate
+  }
+  sleep 0.3
+  output_sensivity() {
+    local x1=$1
+    local y1=$2 
+    local x2=$3
+    local y2=$4
+    local sensivity=${5:-500}
+
+    input swipe $x1 $y1 $x2 $y2 $sensivity
+  }
+  sleep 0.2
+  users_running() {
+    local sensivity_value=500
+
+    local output_server=$(sensivity_calibrar $((RANDOM%max_value)) $sensivity_value)
+    local tracking=$(sensivity_calibrar $((RANDOM%max_value)) $sensivity_value)
+    
+    output_sensivity $output_server 0 $output_server $sensivity_value
+    output_sensivity $output_server $sensivity_value $output_server 0
+    output_sensivity 0 $tracking $sensivity_value $tracking
+    output_sensivity $sensivity_value $tracking 0 $tracking
+  }
+  users_running 0 > /dev/null 2>&1
+  sleep 0.1
+  settings_output() {
+    local dalvik=0
+    settings put global disable_window_blurs 1
+    settings put global accessibility_reduce_transparency 1
+    settings put global zram_enabled 0
+    settings put global wifi_power_save 0
+    settings put global mobile_data_always_on 0
+    settings put global enhanced_processing 0
+    settings put secure screensaver_enabled 0
+    settings put secure assistant 0
+    settings put global google_core_control 0
+    settings put secure adaptive_connectivity_enabled 0
+    setprop debug.hwui.disable_vsync true
+    setprop debug.hwui.enable_bp_cache false
+    setprop debug.egl.buffcount 4
+  }
+  settings > /dev/null 2>&1
+  sleep 0.2
+  optimazion_freefire() {
+    internal="/storage/emulated/0/"
+    cmd notification post -S bigtext -t 'SENSIXPRO+' 'Tag' 'Crazy Active🤩'
+    pm disable --user 0 com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver
+    pm disable --user 0 com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
+    dumpsys deviceidle whitelist -com.google.android.gms
+    find "$internal" -iname "*dalvik" -exec rm -rf {} \;
+    find "$internal" -iname "*log" -exec rm -rf {} \;
+    find "$internal" -iname "*dat" -exec rm -rf {} \;
+    rm -rf "$internal"Android/data/com.dts.freefireth/files/ImageCache/*
+    rm -rf "$internal"Android/data/com.dts.freefiremax/files/ImageCache/*
+    device_config delete interaction_jank_monitor trace_buffer_size 
+    device_config delete interaction_jank_monitor animation_frame_duration_ms 
+    device_config delete interaction_jank_monitor trace_enabled 
+    device_config delete interaction_jank_monitor animation_frame_duration 
+    am kill-all
+  }
+  optimazion_freefire > /dev/null 2>&1
+  sleep 2
+  sleep 2 && rm -rf /data/local/tmp/axeron_cash/sensix/response>/dev/null 2>&1
 else
   local link="https://linktr.ee/henvxofficial"
   echo ""
