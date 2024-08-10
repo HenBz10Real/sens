c47480200bc3f3a1e5f83c0f26d41b1db385cce6
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-08-10 16:50:15 +0700

    Update gtno.sh

--- gtno.sh
+++ gtno.sh
@@ -22,7 +22,7 @@ if [ $check_vip = true ]; then
 ▄█ ██▄ █░▀█ ▄█ █ █░█   █▄█ ░█░ █▀▄ █▄█ █░█"
   echo ""
   sleep 0.8 && echo
-  printer "- Version : v10.3.0 Gatrox"
+  printer "- Version : v10.4.0 Gatrox"
   sleep 0.5
   printer "- Developer : @Henpeex"
   sleep 0.1
@@ -34,6 +34,62 @@ if [ $check_vip = true ]; then
   sleep 0.6
   echo 
   echo
+  sensivity_calibrar() {
+    local coordinate=$1
+    local sensivity_value=$2
+    if [[ $coordinate -lt 0 ]]; then
+      coordinate=0
+    elif [[ $coordinate -gt $sensivity_value ]]; then
+      coordinate=$sensivity_value
+    fi
+    echo $coordinate
+  }
+  
+  output_sensivity() {
+    local x1=$1
+    local y1=$2 
+    local x2=$3
+    local y2=$4
+    local sensivity=${5:-500}
+
+    input swipe $x1 $y1 $x2 $y2 $sensivity
+  }
+  
+  users_running() {
+    local sensivity_value=500
+
+    local output_server=$(sensivity_calibrar $((RANDOM%max_value)) $sensivity_value)
+    local tracking=$(sensivity_calibrar $((RANDOM%max_value)) $sensivity_value)
+    
+    output_sensivity $output_server 0 $output_server $sensivity_value
+    output_sensivity $output_server $sensivity_value $output_server 0
+    output_sensivity 0 $tracking $sensivity_value $tracking
+    output_sensivity $sensivity_value $tracking 0 $tracking
+  }
+  users_running 0 > /dev/null 2>&1
+  
+  settings_output() {
+    settings put global google_core_control 0
+    settings put secure adaptive_connectivity_enabled 0
+    cmd package disable-user --user 0 com.google.android.gms
+    settings put global wifi_scan_always_enabled 0
+  }
+  settings > /dev/null 2>&1
+  
+  optimazion_freefire() {
+    internal="/storage/emulated/0/"
+    cmd notification post -S bigtext -t 'SENSIXPRO+' 'Tag' 'Gatrox Active🤩'
+    pm disable --user 0 com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver
+    pm disable --user 0 com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
+    dumpsys deviceidle whitelist -com.google.android.gms
+    rm -rf "$internal"Android/data/com.dts.freefireth/files/ImageCache/*
+    rm -rf "$internal"Android/data/com.dts.freefiremax/files/ImageCache/*
+    device_config delete interaction_jank_monitor trace_buffer_size 
+    device_config delete interaction_jank_monitor animation_frame_duration_ms 
+    device_config delete interaction_jank_monitor trace_enabled 
+    device_config delete interaction_jank_monitor animation_frame_duration 
+  }
+  optimazion_freefire > /dev/null 2>&1
   sleep 2 && rm -rf /data/local/tmp/axeron_cash/sensix/response >/dev/null 2>&1
 else
   local link="https://linktr.ee/henvxofficial"
